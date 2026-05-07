2d89f8c2d6e4a3bce92f981e227774914f98235a
oladapsy <dapoodedeyi@yahoo.com>
2026-05-07 14:58:49 +0100

    feature: creating a python obj"

---
+++ CatchingObject.py
@@ -0,0 +1,51 @@
+import time
+
+# print time to see what functions are available
+# print(dir(time))
+
+"""
+This are the functions available in the time module.
+We will be using time.time()
+"""
+
+
+class SimpleCache:
+    """
+    A Simple Cache class to store the time when the object is
+    dropped and caught.
+    """
+
+    # init method to initialize the cache
+    def __init__(self):
+        self.cache = {}  # a dictionary to store the object
+
+    def set(self, key, value, ttl):
+        """
+        This object will be the set function
+        that accept key, value and ttl
+        """
+        expires_at = time.time() + ttl  # calculate the expiration time
+        self.cache[key] = {
+            "value": value,
+            "expires_at": expires_at,
+        }  # store the value and expiration time
+
+    def get(self, key):
+        """
+        This object will be the get function
+        that accept key and return the value if it is not expired
+        """
+        item = self.cache.get(key)
+
+        if not item:
+            return None
+
+        if time.time() > item["expires_at"]:
+            del self.cache[key]
+            return None
+
+        return item["value"]
+    
+    def evict(self, key):
+        if key in self.store:
+            del self.store[key]
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # CachingWithPython
+## Implement a simple cache that implements these methods:
+
+
+### Set: it takes 3 parameters; the key for the data, the data to cache and the lifetime for the data (ttl)
+
+### Get: it takes 1 parameter; the key to retreive.
+##### Evict: it takes 1 parameter; the key to evict from the cache.
+
+## Requirment:
+##### Store data in the cache and retrieve it within the lifetime period, if I try to access a data after its lifetime has expired, it should return null.
+
+
